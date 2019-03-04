ddbbe27dae8b890664270227c0f13d67fec367ff
vliang <vghliang@gmail.com>
2019-03-03 22:42:22 -0500

    refactor webapp

--- webapp.py
+++ webapp.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask import render_template
 
-webapp = Flask(__name__)
+app = Flask(__name__)
 
-@webapp.route('/', methods=['GET'])
+@app.route('/', methods=['GET'])
 def index():
 	return render_template('index.html')
 
 if __name__ == '__main__':
-	webapp.debug = True
-	webapp.run()
+	app.debug = True
+	app.run()
