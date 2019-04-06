85946acfe3412c2564ac2c39c9eaa574d59be654
Matt D'Intino <dintino.m@husky.neu.edu>
2019-04-06 13:29:18 -0400

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -32,7 +32,7 @@ app = Flask(__name__)
 def index():
 	cities = pd.read_csv("data/cities.csv")
 	us = json.load(open("data/us.json"))
-	return render_template('index.html', cities=(cities.to_json(orient='records')), us=(us.to_json(orient='records')))
+	return render_template('index.html', cities=(cities.to_json(orient='records')), us=us))
 
 @app.route('/compare/<cat>/<loc>', methods=['GET'])
 def compare(cat, loc):
