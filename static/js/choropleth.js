c1bbc5d6a79e2ed3da6030bb88f46003dc360e92
Matt D'Intino <dintino.m@husky.neu.edu>
2019-04-06 11:16:48 -0400

    choropleth

---
+++ static/js/choropleth.js
@@ -0,0 +1,12 @@
+
+var map = d3.geomap.choropleth()
+    .geofile('/d3-geomap/topojson/countries/USA.json')
+    .projection(d3.geoAlbersUsa)
+    .column('2012')
+    .unitId('fips')
+    .scale(1000)
+    .legend(true);
+
+d3.csv('venture_capital.csv', function(error, data) {
+    map.draw(d3.select('#map').datum(data));
+});
