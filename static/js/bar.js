8588d2dab8a2987f9a4ed75a06677090d674068d
Matt D'Intino <dintino.m@husky.neu.edu>
2019-04-09 18:11:51 -0400

    Update bar.js

--- static/js/bar.js
+++ static/js/bar.js
@@ -1,57 +1,55 @@
-    var w = 500;
-    var h = 200;
-    var yA = 30;
-    var padding=5
-    var barPadding = 1;
 
-      //http://learnjsdata.com/read_data.html
-      //https://bl.ocks.org/caravinden/d04238c4c9770020ff6867ee92c7dac1
-      //https://observablehq.com/@d3/bar-chart
-    d3.csv("/static/data/review_types.csv", function (data) {
+var w = 500;
+var h = 250;
+var padding = 30;
 
-    var xScale = d3.scaleLinear()
-                       .domain([50,500])
-                       .range([padding, w - padding]);
+d3.csv("/static/data/review_types.csv", function(data) {
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+const dataset = data;
+
+var xScale = d3.scaleLinear()
+                     .domain([0, 5000])
+										 .range([padding, w - padding]);
+
+var yScale = d3.scaleLinear()
+                     .domain([0,3000])
+                     .range([h - padding, padding]);
+
+var yAxis = d3.axisLeft()
+                     .scale(yScale);
+var xAxis = d3.axisBottom()
+                     .scale(xScale);
 
 
-    var yScale = d3.scaleLinear()
-                       .domain([40,250])
-                       .range([h - padding, padding]);
+var svg = d3.select("body")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
 
-    var xAxis = d3.axisBottom().scale(xScale);
 
-    var yAxis = d3.axisLeft().scale(yScale);
+svg.selectAll("circle")
+   .data(dataset)
+   .enter()
+   .append("circle")
+   .attr("cx", function(d) {
+        return xScale(d.good);
+   })
+   .attr("cy", function(d) {
+   			console.log(d.bad);
+         return yScale(d.bad);
+   })
+   .attr("r", 4)
+   .attr("fill", "orange");
 
-    var svg = d3.select("#bar")
-        .append("svg")
-        .append("g")
-        .attr("width", w)
-        .attr("height", h);
 
-//http://bl.ocks.org/jfreels/6816504
-//https://observablehq.com/@d3/scatterplot
-    svg.selectAll(".dot")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("class","dot")
-        .attr("cx", function (data) { return xScale(data.good) })
-        .attr("cy", function (data) { return yScale(data.bad) })
-        .attr("r", 40000)
-        .attr("fill", "green");
+svg.append("g")
+    .call(yAxis)
+    .attr("transform", "translate(30)");
 
-    svg.append("g")
-        .call(xAxis)
-        .attr("transform", "translate(0,180)");
+svg.append("g")
+    .call(xAxis)
+    .attr("transform", "translate(0," + (h-20) + ")")
 
-    svg.append("g")
-        .call(yAxis)
-        .attr("transform", "translate(30)");
-    console.log("TEST5");
 
-    console.log(data);
-    console.log(data.good);
-    console.log(data.bad);
-
-    console.log("TEST6");
-  });
+});
