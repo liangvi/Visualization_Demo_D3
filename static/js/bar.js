5aa2410dbb6d31c23a8044fe4332793ef7984d75
Matt D'Intino <dintino.m@husky.neu.edu>
2019-04-09 17:02:51 -0400

    Update bar.js

--- static/js/bar.js
+++ static/js/bar.js
@@ -7,6 +7,8 @@
       //http://learnjsdata.com/read_data.html
       //https://bl.ocks.org/caravinden/d04238c4c9770020ff6867ee92c7dac1
       //https://observablehq.com/@d3/bar-chart
+    d3.csv("/static/data/review_types.csv", function (data) {
+
     var xScale = d3.scaleLinear()
                        .domain([50,500])
                        .range([padding, w - padding]);
@@ -20,44 +22,42 @@
 
     var yAxis = d3.axisLeft().scale(yScale);
 
-
-
     console.log("TEST1");
 
-    d3.csv("/static/data/review_types.csv", function (data) {
 
-      var svg_bar = d3.select("#bar")
-          .append("svg")
-          .attr("width", w)
-          .attr("height", h);
+    var svg_bar = d3.select("#bar")
+        .append("svg")
+        .append("g")
+        .attr("width", w)
+        .attr("height", h);
     //d3.csv("review_types.csv", function (data) {
-      console.log("TEST4");
+    console.log("TEST4");
 
-      console.log(data);
-      console.log(data.good);
-      console.log(data.bad);
+    console.log(data);
+    console.log(data.good);
+    console.log(data.bad);
 
-      console.log("TEST3");
+    console.log("TEST3");
 
-      svg.selectAll("circle")
-         .data(data)
-         .enter()
-         .append("circle")
-         .attr("cx", function(d) {
-              console.log("TEST5");
-              console.log(d.good);
-              console.log("TEST6");
+    svg_bar.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", function(d) {
+            console.log("TEST5");
+            console.log(d.good);
+            console.log("TEST6");
 
               //return xScale(d.good) / 10;
-              return 100;
+          return 100;
          })
          .attr("cy", function(d) {
-              console.log("TEST7");
-              console.log(d.bad);
-              console.log("TEST8");
+           console.log("TEST7");
+           console.log(d.bad);
+           console.log("TEST8");
 
               //return yScale(d.bad) / 10;
-              return 100;
+           return 100;
          })
          .attr("r", 40)
          .attr("fill", "orange");
