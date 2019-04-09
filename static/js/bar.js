fbd21207de9599c84c3f80173a9d3c84bf87054e
Matt D'Intino <dintino.m@husky.neu.edu>
2019-04-09 12:34:35 -0400

    axis

--- static/js/bar.js
+++ static/js/bar.js
@@ -8,10 +8,17 @@
       //http://learnjsdata.com/read_data.html
       //https://bl.ocks.org/caravinden/d04238c4c9770020ff6867ee92c7dac1
       //https://observablehq.com/@d3/bar-chart
+    var xScale = d3.scaleLinear()
+                       .domain([padding,w])
+                       .range([w - padding, padding]);
+
 
     var yScale = d3.scaleLinear()
                        .domain([padding,h])
                        .range([h - padding, padding]);
+
+    var xAxis = d3.axisBottom()
+                       .scale(xScale);
 
     var yAxis = d3.axisLeft()
                        .scale(yScale);
@@ -27,7 +34,7 @@
       .attr("x", d => x(d.category))
       .attr("y", d => y(d.value))
       .attr("height", d => y(0) - y(d.value));
-      
+
   svg.append("g")
       .call(xAxis);
 
