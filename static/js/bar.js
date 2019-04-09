08e5caa40ea3e50819b9878bb640b210b0c03d9c
Matt D'Intino <dintino.m@husky.neu.edu>
2019-04-09 16:46:36 -0400

    Update bar.js

--- static/js/bar.js
+++ static/js/bar.js
@@ -20,20 +20,16 @@
 
     var yAxis = d3.axisLeft().scale(yScale);
 
-    var x = d3.scaleBand()
-	     .rangeRound([0, w])
-	     .padding(padding);
 
-    var y = d3.scaleLinear()
-	     .rangeRound([h, 0]);
-
-    var svg_bar = d3.select("#bar")
-        .attr("width", w)
-        .attr("height", h);
 
     console.log("TEST1");
 
     d3.csv("/static/data/review_types.csv", function (data) {
+
+      var svg_bar = d3.select("#bar")
+          .append("svg_bar")
+          .attr("width", w)
+          .attr("height", h);
     //d3.csv("review_types.csv", function (data) {
       console.log("TEST4");
 
@@ -43,30 +39,35 @@
 
       console.log("TEST3");
 
-    svg_bar.selectAll("circle")
-       .data(data)
-       .enter()
-       .append("circle")
-       .attr("cx", function(d) {
-            console.log(d.good)
-            //return xScale(d.good) / 10;
-            return 100;
-       })
-       .attr("cy", function(d) {
-            console.log(d.bad)
+      svg_bar.selectAll("circle")
+         .data(data)
+         .enter()
+         .append("circle")
+         .attr("cx", function(d) {
+              console.log("TEST5");
+              console.log(d.good);
+              console.log("TEST6");
 
-            //return yScale(d.bad) / 10;
-            return 100;
-       })
-       .attr("r", 40)
-       .attr("fill", "orange");
+              //return xScale(d.good) / 10;
+              return 100;
+         })
+         .attr("cy", function(d) {
+              console.log("TEST7");
+              console.log(d.bad);
+              console.log("TEST8");
 
-//https://stackoverflow.com/questions/34691285/move-x-axis-to-coordinate-0-0-on-the-chart-with-d3-js
-    svg_bar.append("g")
-      .call(xAxis)
-      .attr("transform", "translate(0,180)");
+              //return yScale(d.bad) / 10;
+              return 100;
+         })
+         .attr("r", 40)
+         .attr("fill", "orange");
 
-    svg_bar.append("g")
-      .call(yAxis)
-      .attr("transform", "translate(30)");
+  //https://stackoverflow.com/questions/34691285/move-x-axis-to-coordinate-0-0-on-the-chart-with-d3-js
+      svg_bar.append("g")
+        .call(xAxis)
+        .attr("transform", "translate(0,180)");
+
+      svg_bar.append("g")
+        .call(yAxis)
+        .attr("transform", "translate(30)");
   });
