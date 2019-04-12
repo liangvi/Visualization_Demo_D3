4aa5f1841ee9636b6e8d54b2b0396cb9649194a5
Matt D'Intino <dintino.m@husky.neu.edu>
2019-04-11 23:01:18 -0400

    Update bar.js

--- static/js/bar.js
+++ static/js/bar.js
@@ -8,12 +8,12 @@ var margin = {top: 20, right: 20, bottom: 30, left: 40},
     height = 500 - margin.top - margin.bottom;
 
 // set the ranges
-/*var x = d3.scaleBand()
+var x = d3.scaleBand()
           .range([0, width])
           .padding(0.1);
 var y = d3.scaleLinear()
           .range([height, 0]);
-*/
+
 var svg = d3.select("#bar")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -23,12 +23,12 @@ var svg = d3.select("#bar")
           "translate(" + margin.left + "," + margin.top + ")");
 
 d3.csv("/static/data/states.csv", function(data) {
-  data.sort(function(a, b) {
+  /*data.sort(function(a, b) {
     return d3.descending(a.score, b.score);
   })
   x.domain(data.map(function(d) {
     return d.city;
-  }));
+  }));*/
 
   // Scale the range of the data in the domains
   x.domain(data.map(function(d) { return d.city; }));
