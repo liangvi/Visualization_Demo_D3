3785ac69912d7d292b0d77d76aa8f96539393119
Matt D'Intino <dintino.m@husky.neu.edu>
2019-04-13 15:55:07 -0400

    Update bar.js

--- static/js/bar.js
+++ static/js/bar.js
@@ -4,7 +4,7 @@
 
 // set the dimensions and margins of the graph
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
+    width = 480 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 // set the ranges
