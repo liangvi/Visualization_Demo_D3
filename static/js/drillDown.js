c3ae8aa6be14a9f7e61710a97060149af85beb93
Matt D'Intino <dintino.m@husky.neu.edu>
2019-04-14 17:14:05 -0400

    Update drillDown.js

--- static/js/drillDown.js
+++ static/js/drillDown.js
@@ -1,5 +1,5 @@
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
+    width = 600 - margin.left - margin.right,
     height = 300 - margin.top - margin.bottom;
 
 var barWidth = 5;
